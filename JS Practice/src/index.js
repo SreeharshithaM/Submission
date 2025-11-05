6d00e3c84f69cae508a09b59f7882288229993e8
Sreeharshitha M <Sreeharshitha.m@cspl.com>
2025-11-05 10:08:53 +0530

    Modules

---
+++ JS Practice/src/index.js
@@ -0,0 +1,5 @@
+import * as math from './Modules/Math.js';
+import { stringLength, toUpperCase } from './Modules/String.js';
+
+console.log("Uppercase:" , toUpperCase("Hello World!!"));
+console.log("Add 5 + 3 =", math.add(5,3));
